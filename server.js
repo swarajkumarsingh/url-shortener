51110175c78699b2d63ba037298a04f50826886c
swarajhoster <yuvrajsimasingh@gmail.com>
2022-12-18 23:33:29 +0530

    commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(
 
 mongoose.set("strictQuery", true);
 
-app.set('views', './views');
+app.set('views', path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
