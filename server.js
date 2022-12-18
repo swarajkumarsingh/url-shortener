3b8aaefb3583a2658b9cea4807f9c3c2df341ca7
swarajhoster <yuvrajsimasingh@gmail.com>
2022-12-18 23:30:54 +0530

    c

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", async (req, res) => {
   try {
     const shortUrls = await ShortUrl.find();
-    res.render("index", { shortUrls });
+    res.render("index.ejs", { shortUrls });
   } catch (error) {
     console.log(error);
   }
